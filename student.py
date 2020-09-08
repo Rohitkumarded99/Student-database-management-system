c7e33c07a60d6accf7d22d01877d1d661c9e2482
Rohit <rohit.kmarded99@gmail.com>
2020-09-08 11:06:05 +0530

    Initial commit

--- student.py
+++ student.py
@@ -1,3 +1,8 @@
+__author__      = "Rohit kumar"
+__copyright__   = "Copyright 2020" "This is only for study purpose dont use it for Diffrent purposes"
+
+
+
 from tkinter import *
 from tkinter import ttk
 import pymysql
@@ -10,7 +15,7 @@ class student:
         self.root.title("Student Management System")
         self.root.geometry("1350x700+0+0")
 
-        title=Label(self.root,text="STUDENT MANAGEMENT SYSTEM",bd=10,relief=GROOVE,font=("times new roman",40,"bold"),bg="yellow",fg="red")
+        title=Label(self.root,text="SCHOOL STUDENT MANAGEMENT SYSTEM",bd=8,relief=GROOVE,font=("Yu Gothic UI Semibold",35,"bold"),bg="#F93800",fg="#143D59")
         #fg = forground color (text color)
         title.pack(side=TOP,fill=X)
 
@@ -26,65 +31,64 @@ class student:
         self.search_txt=StringVar()
 
         #-------------manage frame left frame ---------------
-        manage_frame=Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+        manage_frame=Frame(self.root,bd=4,relief=RIDGE,bg="#F93800")
         manage_frame.place(x=20,y=94,width=500,height=600)
 
         #creating inputs feild for manage frame
-        m_title=Label(manage_frame,text="Manage Student",bg="crimson",fg="white",font=("times new roman",30,"bold"))
+        m_title=Label(manage_frame,text="Manage Student",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",30,"bold"))
         m_title.grid(row=0,columnspan=2,pady=10,padx=40)
 
-        lbl_roll=Label(manage_frame,text="Roll No.",bg="crimson",fg="white",font=("times new roman",17,"bold"))
+        lbl_roll=Label(manage_frame,text="Roll No.",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",17,"bold"))
         lbl_roll.grid(row=1,column=0,padx=10,pady=10,sticky="w")
 
-        txt_roll=Entry(manage_frame,textvariable=self.Roll_No_var,width=23,font=("times new roman",17,"bold"),bd=5,relief=GROOVE)
+        txt_roll=Entry(manage_frame,textvariable=self.Roll_No_var,width=23,font=("Yu Gothic UI Semibold",17,"bold"),bd=5,relief=GROOVE)
         txt_roll.grid(row=1,column=1,padx=10,pady=10,sticky="w")
 
-        lbl_name=Label(manage_frame,text="NAME",bg="crimson",fg="white",font=("times new roman",17,"bold"))
+        lbl_name=Label(manage_frame,text="NAME",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",17,"bold"))
         lbl_name.grid(row=2,column=0,padx=10,pady=10,sticky="w")
 
-        txt_name=Entry(manage_frame,textvariable=self.name_var,width=23,font=("times new roman",17,"bold"),bd=5,relief=GROOVE)
+        txt_name=Entry(manage_frame,textvariable=self.name_var,width=23,font=("Yu Gothic UI Semibold",17,"bold"),bd=5,relief=GROOVE)
         txt_name.grid(row=2,column=1,padx=10,pady=10,sticky="w")
 
-        lbl_email=Label(manage_frame,text="E-MAIL",bg="crimson",fg="white",font=("times new roman",17,"bold"))
+        lbl_email=Label(manage_frame,text="E-MAIL",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",17,"bold"))
         lbl_email.grid(row=3,column=0,padx=10,pady=10,sticky="w")
 
-        txt_email=Entry(manage_frame,textvariable=self.email_var,width=23,font=("times new roman",17,"bold"),bd=5,relief=GROOVE)
+        txt_email=Entry(manage_frame,textvariable=self.email_var,width=23,font=("Yu Gothic UI Semibold",17,"bold"),bd=5,relief=GROOVE)
         txt_email.grid(row=3,column=1,padx=10,pady=10,sticky="w")
 
-        lbl_gender=Label(manage_frame,text="GENDER",bg="crimson",fg="white",font=("times new roman",17,"bold"))
+        lbl_gender=Label(manage_frame,text="GENDER",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",17,"bold"))
         lbl_gender.grid(row=4,column=0,padx=10,pady=10,sticky="w")
 
-        #txt_gender=Entry(manage_frame,font=("times new roman",20,"bold"),bd=5,relief=GROOVE)
+        #txt_gender=Entry(manage_frame,font=("Yu Gothic UI Semibold",20,"bold"),bd=5,relief=GROOVE)
         #txt_gender.grid(row=4,column=1,padx=10,pady=10,sticky="w")
         #chosing combo box
-        combo_gender=ttk.Combobox(manage_frame,textvariable=self.gender_var,font=("times new roman",19,"bold"),state='readonly')
+        combo_gender=ttk.Combobox(manage_frame,textvariable=self.gender_var,font=("Yu Gothic UI Semibold",19,"bold"),state='readonly')
         combo_gender['values']=("Male","Female","Other")
         combo_gender.grid(row=4,column=1,padx=10,pady=10,sticky="w")
 
-        lbl_contact=Label(manage_frame,text="CONTACT",bg="crimson",fg="white",font=("times new roman",17,"bold"))
+        lbl_contact=Label(manage_frame,text="CONTACT",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",17,"bold"))
         lbl_contact.grid(row=5,column=0,padx=10,pady=10,sticky="w")
 
-        txt_contact=Entry(manage_frame,textvariable=self.contact_var,width=23,font=("times new roman",17,"bold"),bd=5,relief=GROOVE)
+        txt_contact=Entry(manage_frame,textvariable=self.contact_var,width=23,font=("Yu Gothic UI Semibold",17,"bold"),bd=5,relief=GROOVE)
         txt_contact.grid(row=5,column=1,padx=10,pady=10,sticky="w")
 
-        lbl_dob=Label(manage_frame,text="D.O.B",bg="crimson",fg="white",font=("times new roman",17,"bold"))
+        lbl_dob=Label(manage_frame,text="D.O.B",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",17,"bold"))
         lbl_dob.grid(row=6,column=0,padx=10,pady=10,sticky="w")
 
-        txt_dob=Entry(manage_frame,textvariable=self.dob_var,width=23,font=("times new roman",17,"bold"),bd=5,relief=GROOVE)
+        txt_dob=Entry(manage_frame,textvariable=self.dob_var,width=23,font=("Yu Gothic UI Semibold",17,"bold"),bd=5,relief=GROOVE)
         txt_dob.grid(row=6,column=1,padx=10,pady=10,sticky="w")
 
-        lbl_address=Label(manage_frame,text="ADDRESS",bg="crimson",fg="white",font=("times new roman",17,"bold"))
+        lbl_address=Label(manage_frame,text="ADDRESS",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",17,"bold"))
         lbl_address.grid(row=7,column=0,padx=10,pady=10,sticky="w")
 
-        #txt_address=Entry(manage_frame,font=("times new roman",20,"bold"),bd=5,relief=GROOVE)
+        #txt_address=Entry(manage_frame,font=("Yu Gothic UI Semibold",20,"bold"),bd=5,relief=GROOVE)
         #txt_address.grid(row=7,column=1,padx=10,pady=10,sticky="w")
-        self.txt_address=Text(manage_frame,width=26,height=4,font=("",15))
-        self.txt_address.grid(row=7,column=1,padx=10,pady=10,sticky="w")
-
+        self.txt_address=Text(manage_frame,width=28,height=2,font=("",15))
+        self.txt_address.grid(row=7,column=1,padx=10,pady=20,sticky="w")
 
 #======BUTTON FRAME=======
-        btn_frame=Frame(manage_frame,bd=4,relief=RIDGE,bg="crimson")
-        btn_frame.place(x=20,y=530,width=450)
+        btn_frame=Frame(manage_frame,bd=4,relief=RIDGE,bg="#F93800")
+        btn_frame.place(x=7,y=535,width=450)
 
         addbtn=Button(btn_frame,text="ADD",width=11,command=self.add_student).grid(row=0,column=0,padx=10,pady=10)
         updatebtn=Button(btn_frame,text="UPDATE",width=11,command=self.update_data).grid(row=0,column=1,padx=10,pady=10)
@@ -92,27 +96,29 @@ class student:
         clearbtn=Button(btn_frame,text="CLEAR",width=11,command=self.clear).grid(row=0,column=3,padx=10,pady=10)
 
         #-------------detail frame right frame ---------------
-        detail_frame=Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
+        detail_frame=Frame(self.root,bd=5,relief=RIDGE,bg="#F93800")
         detail_frame.place(x=550,y=94,width=780,height=600)
 
+        m_title=Label(detail_frame,text="Student Database",bg="#F93800",fg="#283350",font=("Yu Gothic UI Semibold",25,"bold"))
+        m_title.grid(row=0,columnspan=6,pady=10,padx=40)
         
-        lbl_search=Label(detail_frame,text="SEARCH BY",bg="crimson",fg="white",font=("times new roman",17,"bold"))
-        lbl_search.grid(row=0,column=0,padx=10,pady=10,sticky="w")
+        lbl_search=Label(detail_frame,text="SEARCH BY",bg="#F93800",fg="#283350",width=10,font=("Yu Gothic UI Semibold",17,"bold"))
+        lbl_search.grid(row=1,column=0,padx=10,pady=10,sticky="w")
 
-        combo_search=ttk.Combobox(detail_frame,textvariable=self.search_by,width=12,font=("times new roman",19,"bold"),state='readonly')
+        combo_search=ttk.Combobox(detail_frame,textvariable=self.search_by,width=10,font=("Yu Gothic UI Semibold",17,"bold"),state='readonly')
         combo_search['values']=("roll_no","name","contact")
-        combo_search.grid(row=0,column=1,padx=10,pady=10,sticky="w")
+        combo_search.grid(row=1,column=1,padx=0,pady=10,sticky="w")
 
-        txt_search=Entry(detail_frame,textvariable=self.search_txt,width=15,font=("times new roman",12,"bold"),bd=5,relief=GROOVE)
-        txt_search.grid(row=0,column=2,padx=10,pady=10,sticky="w")
+        txt_search=Entry(detail_frame,textvariable=self.search_txt,width=15,font=("Yu Gothic UI Semibold",15,"bold"),bd=5,relief=GROOVE)
+        txt_search.grid(row=1,column=2,padx=10,pady=10,sticky="w")
         
-        searchbtn=Button(detail_frame,text="SEARCH",width=12,command=self.search_data).grid(row=0,column=3,padx=10,pady=10)
-        showbtn=Button(detail_frame,text="SHOW",width=12,command=self.fetch_data).grid(row=0,column=4,padx=10,pady=10)
+        searchbtn=Button(detail_frame,text="SEARCH",width=14,height=2,command=self.search_data).grid(row=1,column=3,padx=10,pady=10)
+        showbtn=Button(detail_frame,text="SHOW",width=14,height=2,command=self.fetch_data).grid(row=1,column=4,padx=10,pady=10)
 
         #----------------Table frame-------------------#
         
-        table_frame=Frame(detail_frame,bd=4,relief=RIDGE,bg="crimson")
-        table_frame.place(x=10,y=70,width=750,height=500)
+        table_frame=Frame(detail_frame,bd=4,relief=RIDGE,bg="#F93800")
+        table_frame.place(x=10,y=130,width=750,height=445)
 
         scroll_x=Scrollbar(table_frame,orient=HORIZONTAL)
         scroll_y=Scrollbar(table_frame,orient=VERTICAL)
@@ -121,13 +127,13 @@ class student:
         scroll_y.pack(side=RIGHT,fill=Y)
         scroll_x.config(command=self.student_table.xview)
         scroll_y.config(command=self.student_table.yview)
-        self.student_table.heading("roll",text="roll No.")
-        self.student_table.heading("name",text="name")
-        self.student_table.heading("email",text="email")
-        self.student_table.heading("gender",text="gender")
-        self.student_table.heading("contact",text="contact")
+        self.student_table.heading("roll",text="Roll No.")
+        self.student_table.heading("name",text="Name")
+        self.student_table.heading("email",text="Email")
+        self.student_table.heading("gender",text="Gender")
+        self.student_table.heading("contact",text="Contact")
         self.student_table.heading("dob",text="DOB")
-        self.student_table.heading("address",text="address")
+        self.student_table.heading("address",text="Address")
         self.student_table['show']='headings'
         self.student_table.column("roll",width=50)
         self.student_table.column("name",width=100)
@@ -234,12 +240,6 @@ class student:
                 self.student_table.insert('',END,values=row)
             con.commit()
         con.close()      
-
-
-
-
-
-
 
 root = Tk()
 ob = student(root)
---
+++ students.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2020 at 05:31 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `a`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `Roll_No` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `dob` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`Roll_No`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
